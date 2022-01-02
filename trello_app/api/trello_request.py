9061994a317b1f219900a443b7abebbff4ec00e7
pavel.milanov <pawel.milanov@uandex.ru>
2022-01-02 20:43:40 +0300

    исправлены выводы запросов к API

--- trello_app/api/trello_request.py
+++ trello_app/api/trello_request.py
@@ -7,7 +7,7 @@ env.read_env()
 
 class TrelloRequest:
     """Request for Trello API."""
-    
+            
     params = {
         'key': env('API_KEY'),
         'token': env('API_TOKEN')
@@ -22,8 +22,8 @@ class TrelloRequest:
             f'https://trello.com/1/lists/{self.done_prod}/cards?',
             params=self.params
         )
-        print(r.text)
-        return r.text
+        data = r.json()
+        return data
     
     def get_testing_prod_cards(self):
         """Get cards for Testing PROD list"""
@@ -31,8 +31,8 @@ class TrelloRequest:
             f'https://trello.com/1/lists/{self.testing_prod}/cards?',
             params=self.params
         )
-        print(r.text)
-        return r.text
+        data = r.json()
+        return data
     
     def get_testing_dev_cards(self):
         """Get cards for Testing DEV list"""
@@ -40,8 +40,8 @@ class TrelloRequest:
             f'https://trello.com/1/lists/{self.testing_dev}/cards?',
             params=self.params
         )
-        print(r.text)
-        return r.text        
+        data = r.json()
+        return data        
 
     def archive_done_cards(self):
         """Archive all cards for Done PROD 🎉."""
@@ -56,4 +56,5 @@ class TrelloRequest:
 
 if __name__ == '__main__':
     trello = TrelloRequest()
-    trello.archive_done_cards()
+    # trello.archive_done_cards()
+    trello.get_testing_prod_cards()
