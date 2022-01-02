c3dbbed4404baade9dc6218885ae450b5c108c58
pavel.milanov <pawel.milanov@uandex.ru>
2022-01-02 13:01:10 +0300

    изменена структура проекта

--- trello_app/services/pydocx.py
+++
@@ -1,7 +0,0 @@
-from docxtpl import DocxTemplate
-
-# в документе сделать {{director}}
-doc = DocxTemplate('template.docx')
-context = { 'director' : 'И.И.Иванов'}
-doc.render(context)
-doc.save('report.docx')
