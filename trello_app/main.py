92a050ce1dd7a91fc0c772410cdcb58677cdb969
pavel.milanov <pawel.milanov@yandex.ru>
2022-01-07 15:01:45 +0300

    убрано управление приложением через консоль

--- trello_app/main.py
+++ trello_app/main.py
@@ -1,13 +1,8 @@
-# from api.trello_request import TrelloManager
-from api.google_docs import GoogleDocsManager
-from services.tui import TUI
 from apscheduler.schedulers.background import BackgroundScheduler
 from loguru import logger
 
 
-# trello = TrelloManager()
 scheduler = BackgroundScheduler()
-google_doc = GoogleDocsManager()
 
 logger.add('debug/logs.log', format='{time} {level} {message}',
     level='DEBUG', rotation='100 KB',
@@ -17,6 +12,4 @@ scheduler.start()
 
 if __name__ == '__main__':
 
-    app = TUI().run()
-    # scheduler.add_job(trello.archive_done_cards, 'cron', day_of_week='tue', hour='14')
-    # google_doc.get_document()
+    pass
