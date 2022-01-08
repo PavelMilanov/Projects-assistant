8fb9b22bb283ebd520026f895cc7df51c2696a1f
pavel.milanov <milanov71@icloud.com>
2022-01-08 09:37:30 +0000

    исправлен баг, при котором устанавливалась некорректаня версия nodejs

--- config.sh
+++ config.sh
@@ -11,9 +11,11 @@ sudo pip3 install poetry
 echo "настройка виртуального окружения poetry"
 poetry config virtualenvs.in-project true
 echo "установка NodeJS 16.x"
-url=https://deb.nodesource.com/setup_16.x
-curl -sL {{url}} | sudo bash -
+url=https://deb.nodesource.com/setup_16.x 
+curl -sL {{url}} -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
 sudo apt install -y nodejs
+sudo remove nodesource_setup.sh
 sudo apt install -y npm
 echo 'готово!'
 python3.10 --version
