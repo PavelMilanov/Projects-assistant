bba5486f051e81e29c84db844d4b96cace80bcbf
pavel.milanov <milanov71@icloud.com>
2022-01-08 08:30:46 +0000

    mode

--- trello_app/main.py
+++ trello_app/main.py
@@ -8,6 +8,7 @@ logger.add('debug/logs.log', format='{time} {level} {message}',
     level='DEBUG', rotation='100 KB',
     compression='zip')
 scheduler.start()
+# scheduler.add_job(trello.archive_done_cards, 'cron', day_of_week='tue', hour='14')
 
 
 if __name__ == '__main__':
