0da7f7caeee815bd3a1f1afe9f02b333b8642cee
pavel.milanov <pawel.milanov@yandex.ru>
2022-01-11 00:36:40 +0300

    рефакторинг

--- fastapi/main.py
+++ fastapi/main.py
@@ -17,7 +17,7 @@ auth_scheme = OAuth2PasswordBearer(tokenUrl='/login')
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
+    allow_origins=['http://localhost:8080'],
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
