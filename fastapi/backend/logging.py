5c215948555a6e40e355d20c194da62efe77262f
administrator <milanov71@icloud.com>
2022-01-11 11:37:03 +0300

    fix bug

--- fastapi/backend/logging.py
+++ fastapi/backend/logging.py
@@ -5,6 +5,6 @@ import os
 PATH = os.getcwd()
 sys.path.append(PATH)
 
-logger.add(f'{PATH}debug/logs.log', format='{time} {level} {message}',
+logger.add(f'{PATH}/debug/logs.log', format='{time} {level} {message}',
     level='DEBUG', rotation='100 KB',
     compression='zip')
