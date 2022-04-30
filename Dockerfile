59e93fa23693491cd107882fe4a6e5228a70859e
pavel.milanov <pawel.milanov@yandex.ru>
2022-04-30 12:42:54 +0300

    add start app server and static files on docker container

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM python:buster
+
+COPY server /server
+COPY trello_app /trello_app
+COPY .env /.
+
+RUN pip3 install -r server/requirements.txt
+
+EXPOSE 8000
+
+CMD ["python3", "server/main.py"]
--- server/config.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "установка Python"
-apt install -y python3
-echo "установка Python pip"
-apt install -y python3-pip
-echo "установка poetry"
-pip3 install poetry
-echo "настройка виртуального окружения poetry"
-poetry config virtualenvs.in-project true
